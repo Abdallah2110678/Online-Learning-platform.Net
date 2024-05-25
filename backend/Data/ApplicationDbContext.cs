58fcb9ca398260fdfe1fa8bbe7a2e41d1de8a99f
Moniem <abdulrahman2111656@miuegypt.edu.eg>
2024-05-25 18:57:40 +0300

    Connection database

---
+++ backend/Data/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+using Api.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Api.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        // Define your DbSet properties here
+       public DbSet<User> User { get; set; }
+        public DbSet<Cart> Cart { get; set; }
+        public DbSet<Category> Categorie { get; set; }
+        public DbSet<Course> Course { get; set; }
+        public DbSet<CourseMaterial> CourseMaterial { get; set; }
+        public DbSet<Enrollment> Enrollment { get; set; }
+
+    }
+}
+ 
--- backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
+++ backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bcdc9adc5dd987a6644dad75c8239afc42edb15e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+81e85ac17ea9c884d71a4bd71400ee7a42ea6c47")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
