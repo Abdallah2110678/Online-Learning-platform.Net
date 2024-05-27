81d0052a8b4f83d749bab1aadd20db4362df323b
Moniem <abdulrahman2111656@miuegypt.edu.eg>
2024-05-27 21:20:47 +0300

    course & video dto

---
+++ backend/Dtos/CourseDto.cs
@@ -0,0 +1,18 @@
+
+using backend.Models;
+
+
+namespace backend.Dtos
+{
+    public class CourseDTO
+    {
+        public long Id { get; set; }
+        public string? Title { get; set; }
+        public string? Description { get; set; }
+        public string? Tags { get; set; }
+        public double Price { get; set; }
+        public DateTime Date { get; set; }
+        public string? CategoryName { get; set; }
+        public List<VideoCourseDto> VideoCourses { get; set; }
+    }
+}
---
+++ backend/Dtos/VideoCourseDto.cs
@@ -0,0 +1,13 @@
+
+
+
+
+namespace backend.Dtos
+{
+    public class VideoCourseDto
+    {
+        public long Id { get; set; }
+        public string? Title { get; set; }
+        public string? PathOfVideo { get; set; }
+    }
+}
