1495d347f87c89b41cea29869f0b39a5541fcda4
Abdallah Hamdy <96912938+Abdallah2110678@users.noreply.github.com>
2024-05-27 12:03:06 +0300

    added the Register in Dtos

---
+++ backend/Dtos/Register.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace backend.Dtos
+{
+    public class Register
+    {
+        [StringLength(50)]
+        public string? FName { get; set; }
+        [StringLength(50)]
+        public string? LName { get; set; }
+        public DateTime? DOB { get; set; }
+        public string? Gender { get; set; }
+        public long? Phone { get; set; }
+        [Required]
+        [EmailAddress]
+        public string? Email { get; set; }
+        [Required]
+        public string? Password { get; set; }
+        public Role? Role { get; set; }
+    }
+}
